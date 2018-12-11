0264430427ab293f7de6ceb97df479c029ac4902
danilocaverna <nq3i4fsx@hotmail.com>
2018-12-11 07:57:52 -0300

    tensorboard

--- IA/CPD.py
+++ IA/CPD.py
@@ -5,8 +5,13 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
-
+from tensorflow.keras.callbacks import TensorBoard
 import pickle
+import time
+
+NAME = "plantvillage-disease-{}".format(int(time.time()))
+
+tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
 
 gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
 sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
@@ -41,4 +46,4 @@ model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-model.fit(X, y, batch_size=64, epochs=1, validation_split=0.3)
+model.fit(X, y, batch_size=64, epochs=10, validation_split=0.3, callbacks=[tensorboard])
