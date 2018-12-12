1741b2df6df19f89c9724adb4ba6e150dd5d56df
danilocaverna <nq3i4fsx@hotmail.com>
2018-12-11 22:29:32 -0300

    binary_crossentropy com 2 categorias parece funcionar bem

--- IA/CPD.py
+++ IA/CPD.py
@@ -16,10 +16,10 @@ tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
 gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
 sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 
-pickle_in = open("X.pickle","rb")
+pickle_in = open("X_2class.pickle","rb")
 X = pickle.load(pickle_in)
 
-pickle_in = open("y.pickle","rb")
+pickle_in = open("y_2class.pickle","rb")
 y = pickle.load(pickle_in)
 
 X = X/255.0
@@ -36,14 +36,19 @@ model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
 
-model.add(Dense(64))
-model.add(Activation('relu'))
-
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
 
-model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
 
-model.fit(X, y, batch_size=64, epochs=10, validation_split=0.3, callbacks=[tensorboard])
+class_weight = {0: 0.11632916846186989, 1: 0.12063765618267988}#, 2: 0.05342524773804395, 3: 0.30719517449375267, 4: 0.25247738043946577, 5: 0.2197328737613098, 6: 0.15424386040499785, 7: 0.08229211546747092, 8: 0.2236105127100388, 9: 0.16113744075829384, 10: 0.1917277035760448, 11: 0.2050840155105558, 12: 0.23782852218871176, 13: 0.1809564842740198, 14: 0.07367514002585093, 15: 1.0, 16: 0.4144765187419216, 17: 0.046531667384747954, 18: 0.18440327445066781, 19: 0.24859974149073674, 20: 0.17750969409737183, 21: 0.16372253339077983, 22: 0.027574321413183972, 23: 0.1034037052994399, 24: 0.8259370960792761, 25: 0.32270573028866867, 26: 0.16803102111158982, 27: 0.09220163722533391, 28: 0.3412322274881517, 29: 0.17449375269280482, 30: 0.3127962085308057, 31: 0.15510555794915984, 32: 0.3162429987074537, 33: 0.3102111158983197, 34: 0.23567427832830676, 35: 0.9047824213700991, 36: 0.05988797931925894, 37: 0.2770357604480827}
+
+#sgd = SGD(lr=0.1, decay=1e-2, momentum=0.9)
+#model.compile(loss = 'mean_squared_error', optimizer='sgd', metrics=['accuracy'])
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.fit(
+    X, y, batch_size=32, epochs=20,
+    #class_weight=class_weight,
+    validation_split=0.3,
+    callbacks=[tensorboard])
--- IA/gerarDatasetCPD.py
+++ IA/gerarDatasetCPD.py
@@ -6,7 +6,7 @@ import random
 import pickle
 
 DATADIR = "C:/Users/nq3i4/Documents/dataset/crowdai"
-CATEGORIES = ["c_0","c_1","c_2","c_3","c_4","c_5","c_6","c_7","c_8","c_9","c_10","c_11","c_12","c_13","c_14","c_15","c_16","c_17","c_18","c_19","c_20","c_21","c_22","c_23","c_24","c_25","c_26","c_27","c_28","c_29","c_30","c_31","c_32","c_33","c_34","c_35","c_36","c_37"]
+CATEGORIES = ["c_0","c_1"]#,"c_2","c_3","c_4","c_5","c_6","c_7","c_8","c_9","c_10","c_11","c_12","c_13","c_14","c_15","c_16","c_17","c_18","c_19","c_20","c_21","c_22","c_23","c_24","c_25","c_26","c_27","c_28","c_29","c_30","c_31","c_32","c_33","c_34","c_35","c_36","c_37"]
 IMG_SIZE = 150
 
 training_data = []
@@ -16,7 +16,7 @@ def create_training_data():
         path = os.path.join(DATADIR, category)
         class_num = CATEGORIES.index(category)
         for img in os.listdir(path):
-            img_array = cv2.imread(os.path.join(path,img))
+            img_array = cv2.imread(os.path.join(path,img), cv2.IMREAD_GRAYSCALE)
             new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
             training_data.append([new_array, class_num])
 
@@ -30,14 +30,16 @@ y = []
 for feature, label in training_data:
     X.append(feature)
     y.append(label)
+print(len(X))
+print(len(y))
 
-X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 3)
+X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 
 
-pickle_out = open("X.pickle","wb")
+pickle_out = open("X_2class.pickle","wb")
 pickle.dump(X, pickle_out)
 pickle_out.close()
 
-pickle_out = open("y.pickle","wb")
+pickle_out = open("y_2class.pickle","wb")
 pickle.dump(y, pickle_out)
 pickle_out.close()
