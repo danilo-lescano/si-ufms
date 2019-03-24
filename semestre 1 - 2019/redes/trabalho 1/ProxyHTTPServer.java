a93bb696be987a7bedb5eb7b9df0243f95878b39
danilocaverna <nq3i4fsx@hotmail.com>
2019-03-24 14:08:46 -0400

    trabalho de redes++

--- semestre 1 - 2019/redes/trabalho 1/ProxyHTTPServer.java
+++ semestre 1 - 2019/redes/trabalho 1/ProxyHTTPServer.java
@@ -4,6 +4,21 @@ import java.util.*;
 
 public class ProxyHTTPServer{
 	public static void main(String argv[]) throws Exception{
-        
+		if(argv.length != 1)
+			System.out.println("DEU RUIM");
+
+        GetInetAddress(argv[0]);
+	}
+
+
+	private static InetAddress GetInetAddress(String hostname){
+		InetAddress a = null;
+		try { 
+			a = InetAddress.getByName(hostname);
+			System.out.println(hostname + ": " + a.getHostAddress()); 	
+		} catch (UnknownHostException e) {
+			System.out.println("No address found for " + hostname); 
+		}
+		return a;
 	}
 }
