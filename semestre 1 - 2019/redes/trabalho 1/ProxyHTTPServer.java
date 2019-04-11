15d5acddc74ffcec46339e56158c74922293facd
danilocaverna <nq3i4fsx@hotmail.com>
2019-04-10 23:20:18 -0400

    var trabalho_redes++;

--- semestre 1 - 2019/redes/trabalho 1/ProxyHTTPServer.java
+++ semestre 1 - 2019/redes/trabalho 1/ProxyHTTPServer.java
@@ -6,13 +6,36 @@ public class ProxyHTTPServer{
 	public static void main(String argv[]) throws Exception{
 		if(argv.length != 2)
 			System.out.println("print usage");
-
+		String clientSentence; 
+		String capitalizedSentence;
 		ServerSocket welcomeSocket = new ServerSocket(Integer.parseInt(argv[0]));
 		InetAddress a = welcomeSocket.getInetAddress();
 		//receber porta e tamanho
 		System.out.println(a.getHostAddress() + "\n" + a.getHostName()); 	
 		
+		System.out.println("Address: " + a.getHostAddress());
+		
+		while(true) {
+			Socket connectionSocket = welcomeSocket.accept();
+			InetAddress b = connectionSocket.getInetAddress();
+			
+			System.out.println("Address: " + b.getHostAddress());
+			
+			BufferedReader inFromClient  = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 
+			while (true) {
+				clientSentence = inFromClient.readLine();
+				if (clientSentence == null)
+					break;
+				if (clientSentence.equals("exit"))
+					break;
+				System.out.println(clientSentence);
+				capitalizedSentence = clientSentence.toUpperCase() + '\n';
+				outToClient.writeBytes(capitalizedSentence);
+			}
+			connectionSocket.close();
+		}
 		
         //GetInetAddress(argv[0]);
 	}
