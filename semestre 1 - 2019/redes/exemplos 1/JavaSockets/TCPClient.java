339402ec5a2ba615de2d79eec39cba3772fe00d1
danilocaverna <nq3i4fsx@hotmail.com>
2019-04-15 21:32:00 -0400

    trabalho++

--- semestre 1 - 2019/redes/exemplos 1/JavaSockets/TCPClient.java
+++ semestre 1 - 2019/redes/exemplos 1/JavaSockets/TCPClient.java
@@ -4,20 +4,25 @@ import java.net.*;
 class TCPClient { 
 	public static void main(String argv[]) throws Exception { 
 		String sentence; 
-		String modifiedSentence; 
+		String modifiedSentence;
 
 		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 		InetAddress a = InetAddress.getByName("pudim.com.br");
 		System.out.println(a.getHostAddress());
 
-		Socket clientSocket = new Socket("localhost", 6789);
+		//Socket clientSocket = new Socket(a.getHostAddress(), 80);
+		Socket clientSocket = new Socket("localhost", 8005);
 		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); 
 			
-		sentence = inFromUser.readLine(); 
-		outToServer.writeBytes(sentence + '\n');
+		sentence = "GET /estilo.css HTTP/1.1\r\n";//inFromUser.readLine(); 
+		sentence += "Host: www.pudim.com.br\r\n";//inFromUser.readLine(); 
+		sentence += "\r\n";
+		outToServer.writeBytes(sentence);
+		
+
 		while((modifiedSentence = inFromServer.readLine()) != null){
-			System.out.println("FROM SERVER: " + modifiedSentence);
+			System.out.println(modifiedSentence);
 		}
 		clientSocket.close();
 	} 
